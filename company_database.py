2b14bafe2efad6a07c5ba3f42c813909a8e98a79
AaronKnight91 <a.knight14@hotmail.com>
2020-06-29 23:03:55 +0100

    Creating a database from the scrapped data. Bugs in this code that need to be fixed

---
+++ company_database.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+class CompanyDatabase:
+
+    def __init__(self, db, table):
+        self._table = table
+        self._conn = sqlite3.connect(db)
+        self._cur = self._conn.cursor()
+        self._cur.execute("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY, date TEXT, pe_ratio REAL, ps_ratio REAL, cash_flow REAL, pb_ratio REAL, dividend_yield REAL, payout_ratio REAL)" % self._table)
+        self._conn.commit()
+
+    def insert(self, date, pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio):
+        self._cur.execute("INSERT INTO %s VALUES (NULL, ?, ?, ?, ?, ?, ?)" % self._table,
+                          (date, pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio))
+
+    def view(self):
+        self._cur.execute("SELECT * FROM %s" % self._table)
+        rows = self._cur.fetchall()
+        return rows
+
+    def search(self, date, pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio):
+        self._cur.execute("SELECT * FROM %s WHERE pe_ratio=? OR ps_ratio=? OR cash_flow=? OR pb_ratio=? OR dividend_yield OR payout_ratio=?" % self._table, (pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio))
+        rows = self._cur.fetchall()
+        return rows
+
+    def update(self, id, date, pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio):
+        self._cur.execute("UPDATE %s SET pe_ratio=?, ps_ratio=?, cash_flow=?, pb_ratio=?, dividend_yield=?, payout_ratio=? WHERE id=?" % self._table,
+                         (pe_ratio, ps_ratio, cash_flow, pb_ratio, dividend_yield, payout_ratio,id))
+        self._conn.commit()
+
+    def delete(self, id):
+        self._cur.execute("DELETE FROM %s WHERE id=?" % self._table,(id,))
+        self._conn.commit()
+
+    def __del__(self):
+        self._conn.close()
--- run.py
+++ run.py
@@ -2,16 +2,32 @@ import csv
 import time
 
 from company import Company
+from company_database import CompanyDatabase
 
 def run():
 
     with open("freetrade_uk_shares.csv","r") as csv_file:
         reader = csv.reader(csv_file)
-        for row in reader:
-            if not row[4] == "Investing.com" and not row[4] == "":
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            if not row[4] == "":
+                print(type(row[0]), row[0])
                 company = Company(row[4])
-                print(company._pe_ratio, "\n")
+                print(str(row[0]))
+                cdb = CompanyDatabase("companies.db",str(row[0]))
+                print("DEBUG")
+                cdb.insert("TODAY",
+                           float(company._pe_ratio),
+                           float(company._ps_ratio),
+                           float(company._cash_flow),
+                           float(company._pb_ratio),
+                           float(company._dividend_yield),
+                           float(company._payout_ratio))
+                cdb.__del__()
                 time.sleep(1)
+            else:
+                continue
   
 if __name__ == "__main__":
     run()
