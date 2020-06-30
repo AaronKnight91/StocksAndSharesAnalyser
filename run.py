55417de1bf50305e1c22ebcb7b0b3389d9748d51
AaronKnight91 <a.knight14@hotmail.com>
2020-06-30 14:10:09 +0100

    Creating a pandas DataFrame to analyse after each time the programme is run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import csv
 import time
 from datetime import datetime
+import pandas as pd
 
 from company import Company
 from company_database import CompanyDatabase
@@ -9,10 +10,10 @@ def run():
 
     today = datetime.now().strftime("%Y%m%d")
 
+    l = []
     with open("freetrade_uk_shares.csv","r",encoding='cp1252') as csv_file:
         reader = csv.reader(csv_file)
 
-        l = []
         for i, row in enumerate(reader):
             if i == 0:
                 continue
@@ -21,14 +22,16 @@ def run():
 
             print("Scrapping data for %s" % row[0])
 
-            #d = {}
+            d = {}
             company = Company(row[5])
-            #d = {"pe_ratio":company._pe_ratio,
-            #     "ps_ratio":company._ps_ratio,
-            #     "cash_flow":company._cash_flow,
-            #     "pb_ratio":company._pb_ratio,
-            #     "dividend_yield":company._dividend_yield,
-            #     "payout_ratio":company._payout_ratio}
+            d = {"Company Name": row[0],
+                 "pe_ratio":company._pe_ratio,
+                 "ps_ratio":company._ps_ratio,
+                 "cash_flow":company._cash_flow,
+                 "pb_ratio":company._pb_ratio,
+                 "dividend_yield":company._dividend_yield,
+                 "payout_ratio":company._payout_ratio}
+            l.append(d)
               
             cdb = CompanyDatabase("companies.db",str(row[1]).replace("-","").replace("&","").replace("'",""))
             cdb.insert(today,
@@ -40,9 +43,9 @@ def run():
                        float(company._payout_ratio))
             cdb.__del__()
             time.sleep(1)
-           
-        #for i in l:
-        #    print(i)
+
+    df = pd.DataFrame(l)
+    print(df)
             
 if __name__ == "__main__":
     run()
