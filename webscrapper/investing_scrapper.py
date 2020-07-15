ecb8cf747e3296e6fcb9a37eaf495cee22f8a827
AaronKnight91 <a.knight14@hotmail.com>
2020-07-15 17:22:31 +0100

    Scrapping all income statement attributes from investing.com

--- webscrapper/investing_scrapper.py
+++ webscrapper/investing_scrapper.py
@@ -8,14 +8,43 @@ class InvestingIncomeStatementScrapper(BaseScrapper):
 
     def __init__(self, webpage):
         BaseScrapper.__init__(self, webpage)
-        self._html = self._soup.find_all("td")#"table", {"class":"genTbl reportTbl"})
+        self._html = self._soup.find_all("td")
 
-        year = self._html[7].find_all("th")[2].text # 1 - 4
-        total_revenue = self._html[8].find_all("th")
-        revenue = self._html[10]#.find_all("td")[1].text
-        other_revenue = self._html[11]#.find_all("td")[6].text
-        total_cost_of_revenue = self._html[12]
-
+        self._years = self.get_year()
+        self._total_revenue = self.get_total_revenue()
+        self._revenue = self.get_revenue()
+        self._other_revenue = self.get_other_revenue()
+        self._total_cost_of_revenue = self.get_total_cost_of_revenue()
+        self._gross_profit = self.get_gross_profit()
+        self._total_operating_expenses = self.get_tot_operating_expenses()
+        self._total_general_expenses = self.get_tot_general_expenses()
+        self._research_expenses = self.get_research_expenses()
+        self._depreciation_amortization = self.get_depreciation_amortization()
+        self._interest_expense_income_net_operating = self.get_interest_expense_income_net_operating()
+        self._unusual_expense = self.get_unusual_expense()
+        self._other_operating_expenses = self.get_other_operating_expenses()
+        self._operating_income = self.get_operating_income()
+        self._get_interest_income = self.get_interest_income():
+        self._gain_on_sales_of_assets = self.get_gain_on_sales_of_assets()
+        self._net_other = self.get_net_other()
+        self._net_income_before_taxes = self.get_net_income_before_taxes()
+        self._provision_for_income_taxes = self.get_provision_for_income_taxes()
+        self._net_income_after_taxes = self.get_net_income_after_taxes()
+        self._minority_interest = self.get_minority_interest()
+        self._equity_in_affiliates = self.get_equity_in_affiliates()
+        self._us_gaap_adjustment = self.get_us_gaap_adjustment()
+        self._net_income_before_extraordinary_items = self.get_net_income_before_extraordinary_items()
+        self._total_extraordinary_items = self.get_total_extraordinary_items()
+        self._net_income = self.get_net_income()
+        self._total_adjustments_to_net_income = self.get_total_adjustments_to_net_income()
+        self._income_avaliable_to_common_excluding_extraordinary_items = self.get_income_avaliable_to_common_excluding_extraordinary_items()
+        self._diultion_adjustment = self.get_dilution_adjustment()
+        self._diluted_net_income = self.get_diluted_net_income()
+        self._diluted_weighted_average_shares = self.get_diluted_weighted_average_shares()
+        self._diluted_eps_excluding_extraordinary_items = self.get_diluted_eps_excluding_extraordinary_items()
+        self._dps_comon_stock_primary_issue= self.get_dps_common_stock_primary_issue()
+        self._diluted_normalised_eps = self.get_diluted_normalised_eps()
+    
     def get_year(self):
         years = []
         for i in range(1, 5):
@@ -31,7 +60,263 @@ class InvestingIncomeStatementScrapper(BaseScrapper):
             holder.append(total_revenue)
 
         return holder
-        
+
+    def get_revenue(self):
+        holder = []
+        for i in range(1, 5):
+            revenue = self._html[10].find_all("td")[i].text
+            holder.append(revenue)
+
+        return holder
+
+    def get_other_revenue(self):
+        holder = []
+        for i in range(1, 5):
+            revenue = self._html[11].find_all("td")[i].text
+            holder.append(revenue)
+
+        return holder
+
+    def get_total_cost_of_revenue(self):
+        holder = []
+        for i in range(1, 5):
+            revenue = self._html[12].find_all("td")[i].text
+            holder.append(revenue)
+
+        return holder
+
+    def get_gross_profit(self):
+        holder = []
+        for i in range(1,5):
+            gross_profit = self._html[13].find_all("td")[i].text
+            holder.append(gross_profit)
+
+        return holder
+
+    def get_tot_operating_expenses(self):
+        holder = []
+        for i in range(1,5):
+            tot_op_exp = self._html[14].find_all("td")[i].text
+            holder.append(tot_op_exp)
+
+        return holder
+
+    def get_tot_general_expenses(self):
+        holder = []
+        for i in range(1,5):
+            tot_general_exp = self._html[16].find_all("td")[i].text
+            holder.append(tot_general_exp)
+
+        return holder
+
+    def get_research_expenses(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[17].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_depreciation_amortization(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[18].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_interest_expense_income_net_operating(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[19].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_unusual_expense(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[20].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_other_operating_expenses(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[21].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_operating_income(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[22].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_interest_income(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[23].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_gain_on_sales_of_assets(self):
+        holder = []
+        for i in range(1,5):
+            gain = self._html[24].find_all("td")[i].text
+            holder.append(gain)
+
+        return holder
+
+    def get_net_other(self):
+        holder = []
+        for i in range(1,5):
+            other = self._html[25].find_all("td")[i].text
+            holder.append(other)
+
+        return holder
+
+    def get_net_income_before_taxes(self):
+        holder = []
+        for i in range(1,5):
+            nibt = self._html[26].find_all("td")[i].text
+            holder.append(nibt)
+
+        return holder
+
+    def get_provision_for_income_taxes(self):
+        holder = []
+        for i in range(1,5):
+            pfit = self._html[27].find_all("td")[i].text
+            holder.append(pfit)
+
+        return holder
+
+    def get_net_income_after_taxes(self):
+        holder = []
+        for i in range(1,5):
+            niat = self._html[28].find_all("td")[i].text
+            holder.append(niat)
+
+        return holder
+
+    def get_minority_interest(self):
+        holder = []
+        for i in range(1,5):
+            interest = self._html[29].find_all("td")[i].text
+            holder.append(interest)
+
+        return holder
+
+    def get_equity_in_affiliates(self):
+        holder = []
+        for i in range(1,5):
+            equity = self._html[30].find_all("td")[i].text
+            holder.append(equity)
+
+        return holder
+
+    def get_us_gaap_adjustment(self):
+        holder = []
+        for i in range(1,5):
+            adj = self._html[31].find_all("td")[i].text
+            holder.append(adj)
+
+        return holder
+
+    def get_net_income_before_extraordinary_items(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[32].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_total_extraordinary_items(self):
+        holder = []
+        for i in range(1,5):
+            tot = self._html[33].find_all("td")[i].text
+            holder.append(tot)
+
+        return holder
+
+    def get_net_income(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[34].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_total_adjustments_to_net_income(self):
+        holder = []
+        for i in range(1,5):
+            adj = self._html[35].find_all("td")[i].text
+            holder.append(adj)
+
+        return holder
+
+    def get_income_avaliable_to_common_excluding_extraordinary_items(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[36].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_dilution_adjustment(self):
+        holder = []
+        for i in range(1,5):
+            adj = self._html[37].find_all("td")[i].text
+            holder.append(adj)
+
+        return holder
+
+    def get_diluted_net_income(self):
+        holder = []
+        for i in range(1,5):
+            income = self._html[38].find_all("td")[i].text
+            holder.append(income)
+
+        return holder
+
+    def get_diluted_weighted_average_shares(self):
+        holder = []
+        for i in range(1,5):
+            exp = self._html[39].find_all("td")[i].text
+            holder.append(exp)
+
+        return holder
+
+    def get_diluted_eps_excluding_extraordinary_items(self):
+        holder = []
+        for i in range(1,5):
+            eps = self._html[40].find_all("td")[i].text
+            holder.append(eps)
+
+        return holder
+
+    def get_dps_common_stock_primary_issue(self):
+        holder = []
+        for i in range(1,5):
+            dps = self._html[41].find_all("td")[i].text
+            holder.append(dps)
+
+        return holder
+
+    def get_diluted_normalised_eps(self):
+        holder = []
+        for i in range(1,5):
+            eps = self._html[42].find_all("td")[i].text
+            holder.append(eps)
+
+        return holder
+
 class InvestingBalanceSheetScrapper(BaseScrapper):
 
     def __init__(self, webpage):
