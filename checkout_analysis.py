eb946c90eaf415dc4820e28985dcf578c6b05668
AaronKnight91 <a.knight14@hotmail.com>
2020-10-20 13:58:02 +0100

    Changing script so that no command line arguments are needed to see today's analysis

--- checkout_analysis.py
+++ checkout_analysis.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from datetime import datetime
 import argparse
+import pandas as pd
+
 
 def main(args):
 
@@ -7,17 +9,18 @@ def main(args):
     print(df.head())
     
     
-def check_args():
+def check_args(today):
 
     parser = argparse.ArgumentParser(description='Open analysis files.')
 
     parser.add_argument("--path","-p",type=str,help="The directory in which your csv is saved",default="/home/aaron/investment_app/data/analysis")
-    parser.add_argument("--file","-f",type=str,help="The csv you wish to read into a data frame")
+    parser.add_argument("--file","-f",type=str,help="The csv you wish to read into a data frame",default="%s_analysed_companies.csv" % today)
 
     args = parser.parse_args()
 
     return args
         
 if __name__ == "__main__":
-    args = check_args()
+    today = datetime.today().strftime('%Y%m%d')
+    args = check_args(today)
     main(args)
