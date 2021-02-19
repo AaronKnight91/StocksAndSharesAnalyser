1d8643d429bb3a0b254160123e18794c2476ac16
Dr. Aaron Knight <a.knight14@hotmail.com>
2021-02-19 16:43:58 +0000

    Selenium driver now working. Must add chrome driver to path /usr/local/bin the chmod +x to make it executable

--- webscrapper/income_statement_scrapper.py
+++ webscrapper/income_statement_scrapper.py
@@ -1,17 +1,23 @@
-import Selenium
+from selenium import webdriver
 from base_scrapper import BaseScrapper
 
 class IncomeStatement(BaseScrapper):
 
     def __init__(self, webpage):
         BaseScrapper.__init__(self, webpage)
-        self._html = self._soup.find_all("tr")
+        self._button = self._soup.find_all("a",{"class","newBtn toggleButton LightGray"})
 
+        #button = driver.find_element_by_class_name()
 
+        driver = webdriver.Chrome()
+        driver.get(webpage)
+        print(driver.page_source)
+        
 def main():
 
     i = IncomeStatement("https://uk.investing.com/equities/unilever-ord-income-statement?cid=6661")
-    print(i._html)
+    #print(i._button)
+    #print(i.html)
 
 if __name__ == "__main__":
     main()
